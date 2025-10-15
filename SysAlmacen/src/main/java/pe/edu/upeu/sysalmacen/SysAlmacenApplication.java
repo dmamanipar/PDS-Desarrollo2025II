1cbbc370dfedfdeebbdcbed5467117a59aa81813
noetipo <noe.tipo@gmail.com>
2025-10-15 15:32:54 -0500

    ProbandoA

--- SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/SysAlmacenApplication.java
+++ SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/SysAlmacenApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SysAlmacenApplication {
-
+	//Probar CI con Jenkins
 	public static void main(String[] args) {
 		SpringApplication.run(SysAlmacenApplication.class, args);
 	}
